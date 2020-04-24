0bac22a2fe6d3f2ce8509760ce65f3c2c99ccffd
aether-fiducia <aethersaunders@gmail.com>
2020-04-24 15:29:27 -0300

    Just make it work

--- corvuscorax.js
+++ corvuscorax.js
@@ -1,13 +1,23 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
+var net = require('net)
 const config = require("./config.json")
-// Future...
+
 const serverstatus = config.servername
+const DEFAULT_TIMEOUT = 5
+const hook = net.connect(25565, config.hostname, () => {
+    var buff = buffer.from([0xFE, 0x001]);
+    hook.write(buff);
+});
+hook.setTimmeout(DEFAULT_TIMEOUT * 1000)
+
+client.login(config.token)
 
 // On successful bot launch print to stdout
 client.on("ready", () => {
     console.log("I am ready");
     console.log(client);
+    client.user.setStatus("Players online: " +
 });
 
 // On a message being sent to any channel of a connected server
@@ -18,10 +28,10 @@ client.on("message", (message) => {
     } else
     if (message.content.startsWith(config.prefix + "players")) {
         message.channel.send("Filler for when players are listed");
-    } else
-    if (message.content.startsWith(config.prefix + "deaths")) {
-        message.channel.send("Filler for when death data is shown");
     }
 });
 
-client.login(config.token)
+setInterval(function() {
+
+}, 60 * 1000);
+
--- temptest.js
+++ temptest.js
@@ -1,17 +1,17 @@
 var net = require("net")
 
 const DEFAULT_TIMEOUT = 5; // Seconds
-const client = net.connect(25565, "corvuscorax.org", () =>
+const hook = net.connect(25565, "corvuscorax.org", () =>
 {
     var buff = Buffer.from([0xFE, 0x01]);
-    client.write(buff);
+    hook.write(buff);
 });
 
-client.setTimeout(DEFAULT_TIMEOUT * 1000);
+hook.setTimeout(DEFAULT_TIMEOUT * 1000);
 
-client.on('data', (data) => {
+hook.on('data', (data) => {
     if (data == null) {
-        client.end();
+        hook.end();
         process.exit();
     }
     var process = data.toString().split("\x00\x00\x00");
@@ -20,7 +20,7 @@ client.on('data', (data) => {
     }
 });
 
-client.on('timeout', () => {
-    client.end();
+hook.on('timeout', () => {
+    hook.end();
     process.exit();
 });
